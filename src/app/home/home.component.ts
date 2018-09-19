4845b884687c7da90749f17fc2f9c537d993b0ba
Mohammed Akram Faiz Ahmed <akramfaiz@Mohammeds-MacBook-Pro.local>
2018-09-20 00:21:34 +0530

    home screen anim

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('leafLeft', [
+      transition('void => *',[
+        animate('5s ease-in',keyframes([
+            style({
+              offset: 0,
+              opacity: 0,
+              transform: 'translateX(-100%)'
+            }),
+            style({
+              offset: 1,
+              opacity: 1,
+              transform: 'translateX(20px)'
+            })
+        ]))
+      ])  
+    ]),
+    trigger('leafRight', [
+      transition(':enter',[
+        animate('5s ease-in',keyframes([
+            style({
+              offset: 0,
+              opacity: 0,
+              transform: 'translateX(100%)'
+            }),
+            style({
+              offset: 1,
+              opacity: 1,
+              transform: 'translateX(20px)'
+            })
+        ]))
+      ])  
+    ]) 
+  ]
 })
-export class HomeComponent implements OnInit {
 
+export class HomeComponent implements OnInit {
+  currentState: boolean;
   constructor() { }
 
   ngOnInit() {
+    this.currentState = true;
   }
 
 }

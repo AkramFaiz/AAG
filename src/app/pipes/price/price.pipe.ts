21444c4b036f114fe9bff4f4f15a4aa09a55d810
Mohammed Akram Faiz Ahmed <akramfaiz@Mohammeds-MacBook-Pro.local>
2018-09-20 17:46:58 +0530

    price filter pipe

--- src/app/pipes/price/price.pipe.ts
+++ src/app/pipes/price/price.pipe.ts
@@ -1,17 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { forEach } from '@angular/router/src/utils/collection';
-import { filterQueryId } from '@angular/core/src/view/util';
 
 @Pipe({
   name: 'price'
 })
 export class PricePipe implements PipeTransform {
-  // costVal:any;
   transform(value: any, args?: any): any {
-    console.log(value, args[0]*1000 , args[1]*1000);
-    // value.forEach(function (arrayItem) {
-    //  return this.costVal = this.costVal.push(arrayItem.cost);
-    // })
-    return value;
+    return value.filter(res => res.cost >= args[0]*1000 && res.cost <= args[1]*1000);
   }
 }

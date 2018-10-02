c1a9af45c38fbfdc864898d8df57842754e3f3ae
Mohammed Akram Faiz Ahmed <akramfaiz@Mohammeds-MacBook-Pro.local>
2018-10-02 23:52:18 +0530

    popup in progress

--- src/app/popup/popup.component.ts
+++ src/app/popup/popup.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input, DoCheck, AfterViewChecked} from '@angular/core';
 import { PopupStateService } from '../services/popup-state.service';
 import { CurImageService } from '../services/cur-image.service';
+import { MessageService } from 'primeng/api';
+import { LoginService } from '../services/login.service';
+import { ImageType } from '../interface/image.interface';
 
 @Component({
   selector: 'app-popup',
@@ -10,7 +13,16 @@ import { CurImageService } from '../services/cur-image.service';
 export class PopupComponent implements OnInit , AfterViewChecked {
   isVisibleMiddle = false;
   imageDetail: string;
-  constructor(private popS: PopupStateService, private imgS: CurImageService) { }
+
+  likeCount = 0;
+  likeFlag = false;
+  cartAct = false;
+  selCartList: ImageType[] = [];
+  res: any;
+
+  constructor( private messageService: MessageService,
+     private loginS: LoginService,
+     private popS: PopupStateService, private imgS: CurImageService) { }
   ngOnInit() {
     this.imageDetail = this.imgS.curImg;
     console.log('clicked ngAfterViewChecked', this.imageDetail);
@@ -31,5 +43,27 @@ export class PopupComponent implements OnInit , AfterViewChecked {
   }
   commentSubmit(cmt) {
     console.log(cmt);
+  }
+  cartSelected(sCart) {
+    this.loginS.loginSts.subscribe(res => this.res = res);
+    console.log('lSts: ', this.res);
+    if ( this.res === true ) {
+      this.cartAct = !this.cartAct;
+      this.selCartList.push(sCart);
+      console.log(this.selCartList);
+    } else {
+      this.cartAct = false;
+    this.messageService.add({key: 'cart', severity: 'warn', summary: 'Not yet logged in.', detail: 'Login Required.'});
+    }
+  }
+  clear() {
+      this.messageService.clear();
+  }
+  likeClk(ee) {
+    // if (ee.target.className === 'unlike') {
+    //   return this.imageList.likeCount++;
+    // } else {
+    //   return this.imageList.likeCount--;
+    // }
   }
 }
